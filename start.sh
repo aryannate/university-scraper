0295364adca9ab49c701d458fa1b675f826f3a12
Aryan Nate <134719204+aryannate@users.noreply.github.com>
2025-10-20 02:03:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# Install Playwright browser binaries (needed for browser fallback)
+playwright install
+
+# Now launch your API server
 uvicorn scraper_service:app --host 0.0.0.0 --port $PORT 
